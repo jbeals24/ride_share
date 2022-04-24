83dbc41185e456ec3a52b9de8f8b5737079dcf45
jbeals24 <103008158+jbeals24@users.noreply.github.com>
2022-04-24 19:57:29 -0400

    some changes

--- api/models/user.js
+++ api/models/user.js
@@ -1,5 +1,11 @@
+
+const Exports = module.exports = {};
+
 class User extends Model {
 	static get tableName() {
 		return 'user';
 	}
 }
+
+
+
--- api/test/test-users.js
+++ api/test/test-users.js
@@ -1,4 +1,4 @@
-
+const User = require('../models/user.js');
 
 User.query()
 	.select('id', 'firstName')
