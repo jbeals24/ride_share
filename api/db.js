6012c55cc79efd4d7dc70305ac2e17d2db219cd3
Jada Bonnett <jadabonnett@gmail.com>
2022-04-25 18:26:39 -0400

    more that are done

--- api/db.js
+++ api/db.js
@@ -1,12 +1,16 @@
 const knex = require('knex')({
 	client: 'pg',
 	connection: {
-		host: 'pg.cse.taylor.edu'
-		database: 'jada_bonnett'
+		host: 'pg.cse.taylor.edu',
+		database: 'jada_bonnett',
+		user: 'jada_bonnett',
+		password: 'dizafuqi',
 	}
 });
 
 const objection = require('objection');
-const Model = objection.MOdel;
+const Model = objection.Model;
 Model.knex(knex);
+
+module.exports = {knex, objection, Model};
 
--- api/models/user.js
+++ api/models/user.js
@@ -1,11 +1,48 @@
-
-const Exports = module.exports = {};
+/*
+const {Model} = require('objection');
+const {Driver} = require('../models/driver');
+const {Passenger} = require('../models/passenger');
+const {Ride} = require('../models/ride');
 
 class User extends Model {
 	static get tableName() {
 		return 'user';
 	}
+	static get relationMappings(){
+	return{
+		Driver:{
+			relation: Model.HasManyRelation,
+			modelClass: driver,
+			join:{
+				from: 'user.id',
+				to: 'driver.userId'
+			}
+		}Ride;{
+			relation: Model.HasManyRelation,
+			modelClass: ride,
+			join;{
+				from: 'user.id',
+				through:{
+					from: 'passenger.userId',
+					to: 'passenger.rideId',
+				}to: 'ride.id',
+			}
+		}
+	}
+	}
+}
+const Exports = module.exports = {};
+*/
+
+
+const {Model} = require('objection');
+
+class User extends Model {
+	static get tableName(){
+		return 'user';
+	}
 }
 
+module.exports = {User};
 
-
+//const Exports = module.exports = {};
